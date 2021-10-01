a7b729f22f75750aad1ddcc00f6f691e4fa11587
Reon0429-cat <oonishireon110@gmail.com>
2021-10-01 16:17:00 +0900

    first commit

---
+++ FaceID-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceID-Practice
+//
+//  Created by 大西玲音 on 2021/10/01.
+//
+
+import UIKitz
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
